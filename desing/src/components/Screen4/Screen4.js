f0b33f9f443d96537573150b633dbfc53da0cfb7
Niket Deshmukh <niketdeshmukh2002@gmail.com>
2022-09-11 01:25:11 +0530

    :tada: :octocat:

--- desing/src/components/Screen4/Screen4.js
+++ desing/src/components/Screen4/Screen4.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Screen4.scss";
+import "../Modal/Modal.scss";
 import Header from "../Layout/Header";
 import Footer from "../Footer/Footer";
 import image1 from "../../assets/Screen4/first_img.svg";
@@ -6,8 +8,19 @@ import image3 from "../../assets/Screen3/box1_container2.svg";
 import image4 from "../../assets/Screen3/box2_container2.svg";
 import image5 from "../../assets/Screen3/image__forth.svg";
 import image2 from "../../assets/Screen4/image_four.svg";
-import "./Screen4.scss";
+// import Modal from "../Modal/Modal";
 const Screen4 = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
   return (
     <>
       <div className="container__main">
@@ -36,7 +49,72 @@ const Screen4 = () => {
               the “question mark” icon for details on the exclusive bonus
               available on this package.
             </p>
-            <div className="elipse">?</div>
+            <div className="elipse">
+              <button onClick={toggleModal} className="btn-modal">
+                ?
+              </button>
+            </div>
+
+            {modal && (
+              <div className="moodal">
+                <div onClick={toggleModal} className="overlay"></div>
+                <div className="modal-content">
+                  <h4>
+                    <span style={{ fontSize: "31px" }}>Basic</span>&nbsp; $FURY
+                    Kit
+                  </h4>
+                  <p>
+                    Get 2x $FURY as part of the Membership Bonus. Claim 500
+                    $FURY tokens at just $200!
+                  </p>
+                  <hr
+                    style={{
+                      width: "404px",
+                      height: "0px",
+                      border: "1px solid #EBF243",
+                    }}
+                  />
+                  <p
+                    style={{
+                      color: " #EBF243",
+                      fontFamily: "Sora",
+                      fontStyle: "normal",
+                      fontWeight: "600",
+                      fontSize: "18px",
+                      lineHeight: "135%",
+                      alignItems: "center",
+                    }}
+                  >
+                    {" "}
+                    Bonus deals included in this package:
+                  </p>
+                  <hr
+                    style={{
+                      width: "404px",
+                      marginTop: "-4px",
+                      height: "0px",
+                      border: "1px solid #EBF243",
+                    }}
+                  />
+                  <ul>
+                    <li>Membership Bonus</li>
+                    <li>Early-Bird Bonus</li>
+                  </ul>
+                  <button
+                    style={{
+                      border: "none",
+                      borderRadius: "50%",
+                      width: "33px",
+                      backgroundColor: "white",
+                    }}
+                    className="close-modal"
+                    onClick={toggleModal}
+                  >
+                    X
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <div className="container__box">
@@ -55,7 +133,71 @@ const Screen4 = () => {
               Claim the Pro Kit at 5% discount. Please click the “question mark”
               icon for details on the exclusive bonus available on this package
             </p>
-            <div className="elipse">?</div>
+            <div className="elipse">
+              <button onClick={toggleModal} className="btn-modal">
+                ?
+              </button>
+            </div>
+            {modal && (
+              <div className="moodal">
+                <div onClick={toggleModal} className="overlay"></div>
+                <div className="modal-content">
+                  <h4>
+                    <span style={{ fontSize: "31px" }}>Basic</span>&nbsp; $FURY
+                    Kit
+                  </h4>
+                  <p>
+                    Get 2x $FURY as part of the Membership Bonus. Claim 500
+                    $FURY tokens at just $200!
+                  </p>
+                  <hr
+                    style={{
+                      width: "404px",
+                      height: "0px",
+                      border: "1px solid #EBF243",
+                    }}
+                  />
+                  <p
+                    style={{
+                      color: " #EBF243",
+                      fontFamily: "Sora",
+                      fontStyle: "normal",
+                      fontWeight: "600",
+                      fontSize: "18px",
+                      lineHeight: "135%",
+                      alignItems: "center",
+                    }}
+                  >
+                    {" "}
+                    Bonus deals included in this package:
+                  </p>
+                  <hr
+                    style={{
+                      width: "404px",
+                      marginTop: "-4px",
+                      height: "0px",
+                      border: "1px solid #EBF243",
+                    }}
+                  />
+                  <ul>
+                    <li>Membership Bonus</li>
+                    <li>Early-Bird Bonus</li>
+                  </ul>
+                  <button
+                    style={{
+                      border: "none",
+                      borderRadius: "50%",
+                      width: "33px",
+                      backgroundColor: "white",
+                    }}
+                    className="close-modal"
+                    onClick={toggleModal}
+                  >
+                    X
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <div className="container__box">
